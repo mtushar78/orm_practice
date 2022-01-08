0b9fa79026affc9b660a004e77d7675304907ad7
unknown <mtushar78@gmail.com>
2022-01-08 16:04:30 +0600

    orm  implemented for complex join query

--- src/main/java/orm_practice/practice/repository/BillInfoRepo.java
+++ src/main/java/orm_practice/practice/repository/BillInfoRepo.java
@@ -10,9 +10,11 @@ import java.util.List;
 
 @Repository
 public interface BillInfoRepo extends JpaRepository<BillInfoEntity, Long> {
-    @Query("SELECT new orm_practice.practice.dto.BillInfoSdnJoinDto(bi.billNo, bi.collection_Date, bi.Bank_Txn_Id, bi.Nesco_Txn_Id, bi.total_BillAmount, bi.Principal_Amount, bi.vat_Amount, bi.LPC_Amount, bi.Rev_StampAmount, bi.Net_PrincipalAmount, sl.sndId, sl.sNDName, sl.displayOrder) "
-            + "FROM BillInfoEntity bi JOIN bi.sndList sl where bi.payment_Status = 'PAID'" +
-            "group by sl.sndId, bi.billNo,bi.collection_Date,bi.Bank_Txn_Id,bi.Nesco_Txn_Id,bi.total_BillAmount,bi.Principal_Amount,sl.displayOrder,sl.sNDName order by sl.displayOrder asc")
+    @Query("SELECT new orm_practice.practice.dto.BillInfoSdnJoinDto(bi.billNo, bi.collection_Date, bi.Bank_Txn_Id, bi.client_txn_Id, " +
+            "bi.total_BillAmount, bi.Principal_Amount, bi.vat_Amount, bi.LPC_Amount, bi.Rev_StampAmount, bi.Net_PrincipalAmount, sl.sndId, " +
+            "sl.sNDName, sl.displayOrder) FROM BillInfoEntity bi JOIN bi.sndList sl where bi.payment_Status = 'PAID'" +
+            "group by sl.sndId, bi.billNo,bi.collection_Date,bi.Bank_Txn_Id,bi.client_txn_Id,bi.total_BillAmount,bi.Principal_Amount," +
+            "sl.displayOrder,sl.sNDName order by sl.displayOrder asc")
     List<BillInfoSdnJoinDto> fetchBillInfoSDNJoin();
 
 
